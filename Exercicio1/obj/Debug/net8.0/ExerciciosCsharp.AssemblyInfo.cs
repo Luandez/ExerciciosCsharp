02e54784f10d4b847f57e5947acb50bab3cdbd67
Akilu <124806507+Luandez@users.noreply.github.com>
2025-09-19 17:16:14 +0000

    Mudança de diretório dos arquivos

--- Exercicio1/obj/Debug/net8.0/ExerciciosCsharp.AssemblyInfo.cs
+++ Exercicio1/obj/Debug/net8.0/ExerciciosCsharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExerciciosCsharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdb944749a5cfb3d8d276a2a401e98a280c307d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e4a81d9218ab556fcb7b6abc4f488e78328c32c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExerciciosCsharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExerciciosCsharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
