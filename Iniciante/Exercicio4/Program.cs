a63245533ad06b71879b7604a83eb573bc598f03
Akilu <124806507+Luandez@users.noreply.github.com>
2025-09-24 17:21:16 +0000

    Incluindo o programa de teste - não será o programa de fato

---
+++ Iniciante/Exercicio4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void ExibirMsg()
+    {
+        Console.WriteLine("Ola tudo bem?");
+        Console.WriteLine("Testando");
+    }
+
+    static void ExibirValor(int preco, string nome)
+    {
+        Console.WriteLine(preco);
+        Console.WriteLine($"O nome do cliente: {nome}");
+    }
+
+    static void Main()
+    {
+        ExibirMsg();
+        ExibirValor(20, "Luan");
+    }
+}
