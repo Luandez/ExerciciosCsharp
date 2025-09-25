0c7113944e27905079a8d7e3514e6898bfce5fa7
Akilu <124806507+Luandez@users.noreply.github.com>
2025-09-25 14:01:12 +0000

    Programa que realiza operações de (Quadrado - Retangulo - Circulo)

--- Iniciante/Exercicio4/Program.cs
+++ Iniciante/Exercicio4/Program.cs
@@ -2,21 +2,59 @@
 
 class Program
 {
-    static void ExibirMsg()
+    //Funções que eu criei para realizar os calculos e deixar o codigo mais limpo e legivel
+    static void CalculoQuadrado(double lado)
     {
-        Console.WriteLine("Ola tudo bem?");
-        Console.WriteLine("Testando");
+        double valorQuadrado = lado * lado;
+        Console.WriteLine($"O valor do quadrado: {valorQuadrado}.");
     }
 
-    static void ExibirValor(int preco, string nome)
+    static void CalculoRetangulo(double base1, double altura)
     {
-        Console.WriteLine(preco);
-        Console.WriteLine($"O nome do cliente: {nome}");
+        double valorRetangulo = base1 * altura;
+        Console.WriteLine($"O valor do retangulo: {valorRetangulo}.");
+    }
+
+    static void Circulo(double raio)
+    {
+        double valorCirculo = Math.PI * (Math.Pow(raio, 2));
+        Console.WriteLine($"O valor do circulo: {valorCirculo:f2}.");
     }
 
     static void Main()
     {
-        ExibirMsg();
-        ExibirValor(20, "Luan");
+        //Declaração das variaveis para usar de parametros
+        double num1, num2;
+
+        //apresentação do programa
+        Console.WriteLine("Programa que faz calculo do (quadrado / retangulo / raio)");
+        Console.WriteLine("Escolha um calculo: ");
+        string escolha = Console.ReadLine();
+        escolha = escolha.ToLower();
+
+        //switch para tomar a decisão de qual calculo fazer (quadrado - retangulo ou raio)
+        switch (escolha)
+        {
+            case "quadrado":
+                Console.WriteLine("Escolha o primeiro numero: ");
+                num1 = double.Parse(Console.ReadLine());
+                CalculoQuadrado(num1);
+                break;
+            case "retangulo":
+                Console.WriteLine("Escolha o primeiro numero: ");
+                num1 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Escolha o segundo numero: ");
+                num2 = double.Parse(Console.ReadLine());
+                CalculoRetangulo(num1, num2);
+                break;
+            case "raio":
+                Console.WriteLine("Escolha o primeiro numero: ");
+                num1 = double.Parse(Console.ReadLine());
+                Circulo(num1);
+                break;
+            default:
+                Console.WriteLine("Digite uma operação válida.");
+                break;
+        }
     }
 }
