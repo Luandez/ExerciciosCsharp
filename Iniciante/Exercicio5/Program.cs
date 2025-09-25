f23c1c9c4c75bc08088ca0eb60267d26312adffe
Akilu <124806507+Luandez@users.noreply.github.com>
2025-09-25 18:17:53 +0000

    Programa que faz o fatorial de um numero

---
+++ Iniciante/Exercicio5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void CalculoFatorial(int numero)
+    {
+        int fatorial = 1;
+
+        for (int i = 0; i > 1; i--)
+        {
+            fatorial *= numero;
+            Console.WriteLine($"O fatorial do número {numero} é {fatorial}.");
+        }
+    }
+
+    static void Main()
+    {
+        string continuar;
+        Console.WriteLine("Programa que gera o fatorial do numero selecionado pelo usuario.");
+
+        do
+        {
+            Console.WriteLine("Escolha um numero: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            CalculoFatorial(numero);
+
+            Console.WriteLine("Deseja Continuar o programa (S/N): ");
+            continuar = Console.ReadLine();
+            continuar = continuar.UpperLower();
+        } while (continuar != "N");
+    }
+}
